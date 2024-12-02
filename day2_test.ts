75e94ce15da140d3a9fdfd2a5417b946a9f46fe6
Wolf <wolf.jaeger@sybit.de>
2024-12-02 21:08:34 +0100

    chore(comments): remove unnecessary comments

--- day2_test.ts
+++ day2_test.ts
@@ -35,14 +35,6 @@ Deno.test("freestyle 1", function freestyle1() {
 
 
 /** Tests for the second part */
-/*
-7 6 4 2 1: Safe without removing any level.
-1 2 7 8 9: Unsafe regardless of which level is removed.
-9 7 6 2 1: Unsafe regardless of which level is removed.
-1 3 2 4 5: Safe by removing the second level, 3.
-8 6 4 4 1: Safe by removing the third level, 4.
-1 3 6 7 9: Safe without removing any level.
-*/
 Deno.test("isSecure using problem damper is true for descending", function isSecureWithProblemDamperDescending() {
     assertEquals(isSecureWithProblemDampener(["7", "6", "4", "2", "1"]), true);
 });
