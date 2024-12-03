faaf28919873f99159fee4e316350d19a06827fe
Wolf <wolf.jaeger@sybit.de>
2024-12-03 21:18:03 +0100

    chore: remove naming redundency

--- day2_test.ts
+++ day2_test.ts
@@ -1,31 +1,31 @@
 import { assertEquals } from "@std/assert";
 import { isSecure, isSecureWithProblemDampener} from "./day2.ts";
 
-Deno.test("isSecure is true for having one number", function isSecureForOneNumber() {
+Deno.test("isSecure is true for having one number", () => {
   assertEquals(isSecure(["1"]), true);
 });
 
-Deno.test("isSecure is true for having three number in ascending order", function isSecureForAscNumbers() {
+Deno.test("isSecure is true for having three number in ascending order", () => {
     assertEquals(isSecure(["1", "2", "3"]), true);
 });
 
-Deno.test("isSecure is true for having three number in descending order", function isSecureForAscNumbers() {
+Deno.test("isSecure is true for having three number in descending order", () => {
     assertEquals(isSecure(["2", "1", "0"]), true);
 });
 
-Deno.test("isSecure is false when there is a neither asc nor descending part", function isNotSecureForEqualPart() {
+Deno.test("isSecure is false when there is a neither asc nor descending part", () => {
     assertEquals(isSecure(["2", "1", "1"]), false);
 });
 
-Deno.test("isSecure is false when there is a change from ascending to descending", function isNotSecureForOrderChange() {
+Deno.test("isSecure is false when there is a change from ascending to descending", () => {
     assertEquals(isSecure(["2", "1", "3"]), false);
 });
 
-Deno.test("isSecure is false when two numbers have a bigger diff than 3", function isNotSecureForTwoNumbersWithDiffGtThrees() {
+Deno.test("isSecure is false when two numbers have a bigger diff than 3", () => {
     assertEquals(isSecure(["2", "6"]), false);
 });
 
-Deno.test("isSecure is false when the sequence contains a difference > 3", function isNotSecureForDifferenceGtThree() {
+Deno.test("isSecure is false when the sequence contains a difference > 3", () => {
     assertEquals(isSecure(["9", "7", "6", "2", "1"]), false);
 });
 
@@ -35,26 +35,26 @@ Deno.test("freestyle 1", function freestyle1() {
 
 
 /** Tests for the second part */
-Deno.test("isSecure using problem damper is true for descending", function isSecureWithProblemDamperDescending() {
+Deno.test("isSecure using problem damper is true for descending", () => {
     assertEquals(isSecureWithProblemDampener(["7", "6", "4", "2", "1"]), true);
 });
 
-Deno.test("isSecure using problem damper is false for diff problem", function isNotSecureWithProblemDamperForDiffProblem() {
+Deno.test("isSecure using problem damper is false for diff problem", () => {
     assertEquals(isSecureWithProblemDampener(["1", "2", "7", "8", "9"]), false);
 });
 
-Deno.test("isSecure using problem damper is false for diff problem", function isSecureWithProblemDamperDescending() {
+Deno.test("isSecure using problem damper is false for diff problem", () => {
     assertEquals(isSecureWithProblemDampener(["9", "7", "6", "2", "1"]), false);
 });
 
-Deno.test("isSecure using problem damper is true by removing the second level, 3.", function isSecureWithProblemDamperRemovingSecondLevel() {
+Deno.test("isSecure using problem damper is true by removing the second level, 3.", () => {
     assertEquals(isSecureWithProblemDampener(["1", "3", "2", "4", "5"]), true);
 });
 
-Deno.test("isSecure using problem damper is true by removing the third level, 4.", function isSecureWithProblemDamperRemovingThirdLevel() {
+Deno.test("isSecure using problem damper is true by removing the third level, 4.", () => {
     assertEquals(isSecureWithProblemDampener(["8", "6", "4", "4", "1"]), true);
 });
 
-Deno.test("isSecure using problem damper is true without removing any level.", function isSecureWithProblemDamperRemovingNoLevel() {
+Deno.test("isSecure using problem damper is true without removing any level.", () => {
     assertEquals(isSecureWithProblemDampener(["1", "3", "6", "7", "9"]), true);
 });
