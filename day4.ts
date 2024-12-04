b366a06b321f3309918fb540be94148d5e4258c8
Wolf <wolf.jaeger@sybit.de>
2024-12-05 00:29:09 +0100

    feat(day4): first approach for the second part (WIP)

--- day4.ts
+++ day4.ts
@@ -19,6 +19,48 @@ export function checkXmasOcurrences(matrix:string[][]): number {
     return numXmasOccurences;
 }
 
+export function checkCrossedMasOcurrences(matrix:string[][]): number {
+    let numXmasOccurences = 0;
+    for(let rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
+        for(let colIndex = 0; colIndex < matrix[rowIndex].length; colIndex++) {
+            if (matrix[rowIndex][colIndex] == 'A') {
+                const upRightS = checkNext('diag-up-right', 'S', matrix, rowIndex, colIndex, -1, 1) === 1;
+                const upLeftS = checkNext('diag-up-left', 'S', matrix, rowIndex, colIndex, -1, -1) === 1;
+                const downLeftS = checkNext('diag-down-left', 'S', matrix, rowIndex, colIndex, 1, -1) === 1;
+                const downRightS = checkNext('diag-down-right', 'S', matrix, rowIndex, colIndex, 1, 1) === 1;
+
+                const upRightM = checkNext('diag-up-right-M', 'M', matrix, rowIndex, colIndex, -1, 1) === 1;
+                const upLeftM = checkNext('diag-down-left-M', 'M', matrix, rowIndex, colIndex, -1, -1) === 1;
+                const downLeftM = checkNext('diag-down-left-M', 'M', matrix, rowIndex, colIndex, 1, -1) === 1;
+                const downRightM = checkNext('diag-down-right-M', 'M', matrix, rowIndex, colIndex, 1, 1) === 1;
+         
+                if (upRightS && downLeftM && upLeftS && downRightM) {
+                    //  S   S
+                    //    A
+                    //  M   M
+                    numXmasOccurences++;
+                } else if (upRightM && upLeftM && downRightS && downLeftS) {
+                    //  M   M
+                    //    A
+                    //  S   S
+                    numXmasOccurences++;
+                } else if (upRightM && downLeftS && upLeftS && downRightM) {
+                    //  S   M
+                    //    A
+                    //  S   M
+                    numXmasOccurences++;
+                } else if (upLeftM && downRightS && downLeftM && upRightM) {
+                    //  M   S
+                    //    A
+                    //  M   S
+                    numXmasOccurences++;
+                }  
+             }
+        }
+    }
+    return numXmasOccurences;
+}
+
 function checkNext(direction: string, checkString: string, matrix: string[][], rowStart: number, colStart: number, rowStepLength: number, colStepLength: number): number {
     let hasCheckString = 1;
     const maxRow = matrix.length -1;
@@ -46,13 +88,16 @@ function checkNext(direction: string, checkString: string, matrix: string[][], r
 }
 
 if (import.meta.main) {
-    //const lines = await loadPuzzleRows('./data/aoc_4_1_example.txt');
-    const lines = await loadPuzzleRows('./data/aoc_4_1.txt');
+    const lines = await loadPuzzleRows('./data/aoc_4_1_example.txt');
+    //const lines = await loadPuzzleRows('./data/aoc_4_1.txt');
     const matrix = []
     for(const line of lines) {
         matrix.push([...line[0]]);
     }
     const numberOfXmas = checkXmasOcurrences(matrix);
+    const numberOfMasCrossed = checkCrossedMasOcurrences(matrix);
 
     console.log('Found', numberOfXmas, 'XMAS occurences.')
+    console.log('Found', numberOfMasCrossed, 'X-MAS occurences.')
+
 }
